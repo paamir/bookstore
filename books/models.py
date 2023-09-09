da95e5cf9e9642288d68582fb2d1005dd72a19eb
paaamir <apr12385apsm@gmail.com>
2023-09-09 21:39:46 +0330

    create commentModel

--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth import get_user_model
 
 class Book(models.Model):
     title = models.CharField(max_length=200, blank=False, null=False)
@@ -11,8 +11,19 @@ class Book(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     visibility = models.BooleanField(default=True)
     cover = models.ImageField(upload_to='covers/', blank=True, null=False)
+
     def __str__(self):
         return f'{self.author}:{self.title}'
 
     def get_absolute_url(self):
         return reverse('book_details', args=[self.id])
+
+
+class Comment(models.Model):
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(to=Book, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.name}: {self.text}'
