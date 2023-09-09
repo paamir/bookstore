632125c2983435b444dc1883cf8ce7ba2a65c249
paaamir <apr12385apsm@gmail.com>
2023-09-09 11:22:04 +0330

    make books model and migrate id data base

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=9, decimal_places=1)
+    description = models.TextField(blank=False, null=False)
+    release_date = models.DateField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author}:{self.title}'
+
