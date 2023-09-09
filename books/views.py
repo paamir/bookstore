ad301bb073172f847c058b1295e1a2e7fadd79f0
paaamir <apr12385apsm@gmail.com>
2023-09-09 11:32:56 +0330

    books list view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book
 
-# Create your views here.
+
+class BooksListView(generic.ListView):
+    model = Book
+    template_name = 'books/books_list.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        return Book.objects.all().order_by('-creation_date')
+
+
